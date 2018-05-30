fe8710167245c6044da6d19f7935d539bc9398f4
Kenta Saito <kenta.saito@sagittar.org>
2018-05-30 14:48:20 +0900

    2018-05-30 14:48:20

--- vendor/pieni/site/views/response.php
+++ vendor/pieni/site/views/response.php
@@ -12,6 +12,7 @@
   </head>
   <body>
 <?php \pieni\utility\Utility::loadView(\pieni\core\REQUEST['method'], $vars, str_repeat (' ', 4)); ?>
+<?php load_view(c('request.method'), $vars, str_repeat(' ', 4)); ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
